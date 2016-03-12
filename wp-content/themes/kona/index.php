d76164e3a76e260f82274305f4f243caf398bc2a
Shavit <stmmsn@gmail.com>
2016-03-12 13:12:13 +0000

    custom form

--- wp-content/themes/kona/index.php
+++ wp-content/themes/kona/index.php
@@ -31,7 +31,7 @@
                 <?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
                 
                 <?php the_post_thumbnail(); ?>
-
+                
                 <?php the_excerpt(); ?>
                 
                 
