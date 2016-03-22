ea28e521ef9c3ca1da252eff37157726f0f286c4
Shavit <stmmsn@gmail.com>
2016-03-22 17:20:08 +0000

    new style

--- wp-content/themes/kona/functions.php
+++ wp-content/themes/kona/functions.php
@@ -19,3 +19,27 @@ function kona_setup() {
 add_action( 'after_setup_theme', 'kona_setup' );
 
 endif;
+
+/**
+ * Enqueues scripts and styles.
+ *
+ */
+function kona_scripts() {
+	// libraries
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/jquery.min.js', array( 'jquery' ), '20151204', true );
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/jquery.dropotron.min.js', array( 'jquery' ), '20151204', true );
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/jquery.scrollgress.min.js', array( 'jquery' ), '20151204', true );
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/skel.min.js', array( 'jquery' ), '20151204', true );
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/util.js', array( 'jquery' ), '20151204', true );
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '20151204', true );
+
+	// main functions
+	wp_enqueue_script( 'kona-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20151204', true );
+
+	wp_localize_script( 'kona-script', 'screenReaderText', array(
+		'expand'   => __( 'expand child menu', 'kona' ),
+		'collapse' => __( 'collapse child menu', 'kona' ),
+	) );
+}
+add_action( 'wp_enqueue_scripts', 'kona_scripts' );
+
